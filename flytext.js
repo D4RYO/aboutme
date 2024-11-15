1dacad5db53b3c5e3593443a38d2b1819d35aaca
D4RYO <dario.knezevic@hotmail.com>
2024-11-15 02:54:06 +0100

    flying in effect

--- flytext.js
+++ flytext.js
@@ -1,15 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-            } else {
-                entry.target.classList.remove('visible');
-            }
-        });
-    });
+                const observer = new IntersectionObserver(entries => {
+                    entries.forEach(entry => {
+                        if (entry.isIntersecting) {
+                            entry.target.classList.add('visible');
+                        } else {
+                            entry.target.classList.remove('visible');
+                        }
+                    });
+                });
 
-    document.querySelectorAll('.fly-in-text').forEach(element => {
-        observer.observe(element);
-    });
-});
+                document.querySelectorAll('.fly-in-text').forEach(element => {
+                    observer.observe(element);
+                element.classList.add('fly-out-text');
+                });
+            });
