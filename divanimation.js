9457a7e1a3550f443321ef211d7d349267d39b07
Dario Knezevic <dario-knezevic@hotmail.com>
2024-12-23 23:23:05 +0100

    Delete divanimation.js
    no divination

--- divanimation.js
+++
@@ -1,5 +0,0 @@
-// Variable to control the speed of the fade out animation
-        var fadeOutSpeed = 1; // Speed in seconds
-
-        // Apply the fade out speed to the animation
-        document.getElementById('intro-overlay').style.animationDuration = fadeOutSpeed + 's';
